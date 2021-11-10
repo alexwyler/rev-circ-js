61e3814a041fc8186021026618ace530dc4b82f2
Alex Wyler <alex.s.wyler@gmail.com>
2021-11-10 01:14:23 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "rev-circ-js",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+  ],
+};
